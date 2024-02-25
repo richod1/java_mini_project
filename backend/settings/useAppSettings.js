07833608afc69367d90026bea242094c11c680aa
richod1 <frimpsup@gmail.com>
2024-02-25 04:33:35 +0000

    settings added

---
+++ backend/settings/useAppSettings.js
@@ -0,0 +1,8 @@
+const useAppSettings=()=>{
+    let settings={};
+    settings['group9_app_email']='noreply@group9.com';
+    settings['group9_base_url']='https://cloud.group9.com'; //this is a fake domain
+    return settings;
+}
+
+module.exports=useAppSettings;
---
+++ backend/settings/useDate.js
@@ -0,0 +1,10 @@
+const useDate=({settings})=>{
+    const {group9}=settings;
+    const dateFormat=group9;
+
+    return {
+        dateFormat,
+    }
+}
+
+module.exports=useDate;
