a3fddc151bc1ae81b74083b3f0d81f6d612111bc
richod1 <frimpsup@gmail.com>
2024-02-26 23:00:55 +0000

    controller migrate controller added

---
+++ backend/controllers/appControllers/clientController/read.js
@@ -0,0 +1,26 @@
+const {migrate}=require("./migrate")
+
+const read=async (Model,req,res)=>{
+    let result=await Model.findOne({
+        _id:req.params.id,
+        removed:false,
+    }).exec();
+
+    if(!result){
+        return res.status(404).json({
+            success:false,
+            result:null,
+            message:'No document found',
+        })
+    }else{
+        const migrateData=migrate(result);
+
+        return res.status(200).json({
+            success:true,
+            result:migrateData,
+            message:'we found this document',
+        })
+    }
+}
+
+module.exports=read;
