b25b15b34c44efea1af3636ab5892154fd9cbef9
richod1 <frimpsup@gmail.com>
2024-03-04 16:44:16 +0000

    payment controller update

---
+++ backend/controllers/appControllers/paymentController/index.js
@@ -0,0 +1,18 @@
+const createCRUDController=require("../../middlewaresControllers/createCRUDController")
+const methods=createCRUDController('Payment')
+
+const create=require("./create");
+const summary=require("./summary")
+const update=require('./update')
+const remove=require("./remove")
+const mail=require("./sendMail")
+
+
+methods.mail=sendMail;
+methods.create=create;
+methods.update=update,
+methods.delete=remove;
+methods.summary=summary;
+
+
+module.exports=methods;
